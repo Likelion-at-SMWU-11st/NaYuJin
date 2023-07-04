a3a97339fa235810558a471c240b709570c533e0
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-04 20:49:37 +0900

    [Feat] Admin 커스터마이징

--- session/week07/posts/admin.py
+++ session/week07/posts/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-
+from .models import Comment
 from .models import Post
 
-admin.site.register(Post)
-# Register your models here.
+admin.site.register(Comment)
+
+class CommentInline(admin.TabularInline):
+    model=Comment
+    extra=5
+    min_num=3
+    max_num=5
+    verbose_name='댓글'
+    verbose_name_plural='댓글'
+
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display=['id','image','content','created_at','view_count','write']
+    search_fields=['id','writer_username']
+    search_help_text='게시판 번호, 작성자 검색이 가능합니다'
+    list_filter=['created_at',]
+    readonly_fields=['view_count','created_at']
+    inlines=[CommentInline,]
+
+
