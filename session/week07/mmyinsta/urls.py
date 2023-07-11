5e3daf3e5924f722c158bdb1ba5da8e597871d1f
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-11 21:00:35 +0900

    [week09] template과 view

--- session/week07/mmyinsta/urls.py
+++ session/week07/mmyinsta/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from posts.views import url_view, function_view
+from posts.views import url_parameter_view, index
+from posts.views import class_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('url/',url_view),
+    path('url/<int:username>/',url_parameter_view),
+    path('fbv/',function_view),
+    path('cbv/',class_view.as_view()),
+    path('',index,name='index'),
+    path('posts/',include('posts.urls',namespace='posts')),
 ]
--- session/week07/posts/views.py
+++ session/week07/posts/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+
+from django.views.generic import ListView
+from .models import Post
+
+class class_view(ListView):
+    model=Post
+    template_name='cbv_view.html'
+
+def index(request):
+    return render(request, 'index.html')
+
+def post_list_view(request):
+    return render(request,'posts/post_list.html')
+
+def post_detail_view(request,id):
+    return render(request,'posts/post_detail.html')
+
+def post_create_view(request):
+    return render(request, 'posts/post_form.html')
+
+def post_update_view(request,id):
+    return render(request,'posts/post_update.html')
+
+def post_delete_view(request,id):
+    return render(request,'posts/post_confirm_delete.html')
+
+def url_view(request):
+    data = {'code':'001', 'msg':'OK'}
+    # return HttpResponse(data)
+    return HttpResponse('<h1>url_views</h1>')
+
+
+def url_parameter_view(request,username):
+    print('url_parameter_view()')
+    print(f'username: {username}')
+    print(f'request.GET: {request.GET}')
+    # print(username)
+    # print(request.GET)
+    return HttpResponse(username)
+
+def function_view(request):
+    print(f'request.method: {request.method}')
+    print(f'request.GET: {request.GET}')
+    print(f'request.POST: {request.POST}')
+    return render(request,'view.html')
