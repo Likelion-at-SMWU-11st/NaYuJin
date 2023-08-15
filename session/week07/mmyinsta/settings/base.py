580ffb7d1dd8d296dce737b934d33718ebe6001e
Nayujin <00nyj@sookmyung.ac.kr>
2023-08-15 16:41:19 +0900

    [Fix] settings 파일 분리 취소

--- session/week07/mmyinsta/settings/base.py
+++ session/week07/mmyinsta/settings/base.py
@@ -23,10 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^vpavolaleyw$_efvwl8n1ftsr2ay!1pw^z57tx3ioc43r-yqk'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -73,15 +70,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mmyinsta.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
